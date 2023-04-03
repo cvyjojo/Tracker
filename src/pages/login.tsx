9a7bde01f079e37c53cc69d6a92bf5207710fc3d
jojoacvy <30833273+cvyjojo@users.noreply.github.com>
2023-04-03 14:34:24 +0530

    removing props

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,7 +2,7 @@
 import { signIn, signOut, useSession } from "next-auth/react";
 import Image from "next/image";
 
-const login = (props) => {
+const login = () => {
   const { data: session } = useSession();
   console.log(session);
   if (session) {
