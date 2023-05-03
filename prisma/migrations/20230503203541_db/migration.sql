06b00e9a28c1d4728f601ebafe8cf978dcaf1c11
jojoacvy <jojoacvy@gmail.com>
2023-05-04 02:08:47 +0530

    db update

--- prisma/migrations/20230503203541_db/migration.sql
+++ prisma/migrations/20230503203541_db/migration.sql
@@ -1,4 +1,13 @@
 -- CreateTable
+CREATE TABLE "Example" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Example_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "Account" (
     "id" TEXT NOT NULL,
     "userId" TEXT NOT NULL,
